21cad4d860a9432a1e458d2c16acc138954afaeb
Miguel Rodriguez <mrodriguez@kenzan.com>
2017-11-04 10:10:24 +0100

    Initial draft

---
+++ README.md
@@ -0,0 +1,120 @@
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/bbd8890c66d249d8838b9e64e5ee2327)](https://www.codacy.com/app/conrado-m/workbook?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=conrado-m/workbooks&amp;utm_campaign=Badge_Grade)
+[![Build Status](https://travis-ci.org/conrado-m/workbooks.svg?branch=master)](https://travis-ci.org/conrado-m/workbooks)
+[![Coverage Status](https://coveralls.io/repos/github/conrado-m/workbooks/badge.svg?branch=master)](https://coveralls.io/github/conrado-m/workbooks?branch=master)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+
+<img src="./doc/streaming-icon.jpg?raw=true" width="20%" align="right" style="border:20px solid white">
+
+
+# Excel Workbook Handler
+
+## 💡 Synopsis
+The WorkbookData class uses the Apache POI project to load data from an Excel spreadsheet and transform it into a Collection. A CustomMapper class might be added for a cleaner translation between the data and the logic. See an example in the section Usage below.
+
+## 💾 Installation
+
+To install the Java client library to your local Maven repository, simply execute:
+
+```shell
+git clone --recursive git@github.com:crgz/workbook.git
+cd workbook
+mvn install
+```
+
+Refer to the [official documentation](https://maven.apache.org/plugins/maven-deploy-plugin/usage.html) for more information.
+
+### Maven users
+
+Add this dependency to your project's POM:
+
+```xml
+    <dependency>
+      <groupId>com.github.crgz</groupId>
+      <artifactId>workbook</artifactId>
+      <version>1.0</version>
+      <scope>test</scope>
+    </dependency>
+```
+
+### Gradle users
+
+```groovy
+compile 'com.github.crgz:workbook:1.0'
+```
+
+### Others
+
+At first generate the JAR by executing:
+
+```
+git clone --recursive git@github.com:crgz/workbook.git
+cd workbook
+mvn package
+```
+
+Then manually install the following JARs:
+
+* target//workbook-1.0.jar
+
+## ▶️ Usage
+
+```Java
+public class AggregatorTest
+{
+...
+
+	@Before
+	public void setUp() throws IOException, WorkbookDataException
+	{
+		this.data = new WorkbookData.Builder("src/test/resources/sample.xls").sheet(0).build().collection();
+	}
+
+	@Test
+	public void testAggregator() throws IOException, WorkbookDataException
+	{
+
+
+		this.data.stream().map(CustomMapper::map).forEach(x ->
+		{
+			final Foo event = x.getKey();
+			
+			this.resource.accept(event);
+						
+			final Baz expected = x.getValue();
+			final Baz actual = this.resource.get();
+			assertEquals(expected, actual);
+		});
+	}
+}
+```
+Sample CustomMapper:
+
+```Java
+
+public class EventStatisticsMapper
+{
+	public static SimpleEntry<Event, Statistics> map(Object[] x)
+	{
+		final long time = ((Double) x[0]).longValue();
+		final int value = ((Double) x[1]).intValue();
+		final long sum = ((Double) x[3]).longValue();
+		final long count = ((Double) x[4]).longValue();
+		double avg = (double) sum / count;
+		final long max = ((Double) x[5]).longValue();
+		final long min = ((Double) x[6]).longValue();
+		return new SimpleEntry<Foo, Baz>(new Foo(time * 1000, value), new Baz(sum, count, max, min, avg));
+	}
+}
+```
+
+## 🎁 Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## 🎓 License
+This experiment is released under the [Apache-2.0](https://opensource.org/licenses/Apache-2.0) license.
---
+++ src/main/java/com/github/crgz/workbook/WorkbookData.java
@@ -0,0 +1,180 @@
+/*******************************************************************************
+ * Copyright (C) 2017, Conrado M. Rodriguez
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+package com.github.crgz.workbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.CellValue;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+/**
+ * Read data in an Excel spreadsheet and return it as a collection of objects.
+ * This is designed to facilitate for parameterized tests in JUnit that
+ * get data from an excel spreadsheet.
+ * 
+ * @author Conrado M.
+ */
+public class WorkbookData
+{
+	private HSSFWorkbook workbook;
+	private int index;
+
+	private WorkbookData(Builder builder)
+	{
+		this.workbook = builder.workbook;
+		this.index = builder.index;
+	}
+
+	public static final class Builder
+	{
+		private HSSFWorkbook workbook;
+		private int index = 0;
+
+		public Builder(String path) throws FileNotFoundException, IOException
+		{
+			this.workbook = new HSSFWorkbook(new FileInputStream(path));
+		}
+
+		public Builder sheet(final int index)
+		{
+			this.index = index;
+			return this;
+		}
+
+		public WorkbookData build() throws WorkbookDataException
+		{
+			return new WorkbookData(this);
+		}
+	}
+
+	/**
+	 * The contents of the spreadsheet, in a form compatible with JUnit 4 parameterized tests.
+	 */
+	public Collection<Object[]> collection() throws WorkbookDataException
+	{
+		Sheet sheet = this.workbook.getSheetAt(this.index);
+
+		List<Object[]> rows = new ArrayList<Object[]>();
+		List<Object> rowData = new ArrayList<Object>();
+
+		for (Row row : sheet)
+		{
+			if (!isEmpty(row))
+			{
+				rowData.clear();
+				for (int column = 0; column < countNonEmptyColumns(sheet); column++)
+				{
+					Cell cell = row.getCell(column);
+					rowData.add(objectFrom(workbook, cell));
+				}
+				rows.add(rowData.toArray());
+			}
+			else
+			{
+				break;
+			}
+		}
+		return rows;
+	}
+
+	private boolean isEmpty(final Row row)
+	{
+		Cell firstCell = row.getCell(0);
+		return (firstCell == null) || (firstCell.getCellTypeEnum() == CellType.BLANK);
+	}
+
+	/**
+	 * Count the number of columns, using the number of non-empty cells in the
+	 * first row.
+	 */
+	private int countNonEmptyColumns(final Sheet sheet)
+	{
+		int columnCount = 0;
+		for (Cell cell : sheet.getRow(0))
+		{
+			if (cell.getCellTypeEnum() == CellType.BLANK)
+			{
+				return columnCount;
+			}
+			columnCount++;
+		}
+		return columnCount;
+	}
+
+	private Object objectFrom(final HSSFWorkbook workbook, final Cell cell) throws WorkbookDataException
+	{
+		switch (cell.getCellTypeEnum())
+		{
+		case STRING:
+			return cell.getRichStringCellValue().getString();
+		case NUMERIC:
+			return getNumericCellValue(cell);
+		case BOOLEAN:
+			return cell.getBooleanCellValue();
+		case FORMULA:
+			return evaluateCellFormula(workbook, cell);
+		default:
+			throw new WorkbookDataException();
+		}
+	}
+
+	private Object getNumericCellValue(final Cell cell)
+	{
+		return DateUtil.isCellDateFormatted(cell) ? new Date(cell.getDateCellValue().getTime()) : cell.getNumericCellValue();
+	}
+
+	private Object evaluateCellFormula(final HSSFWorkbook workbook, final Cell cell) throws WorkbookDataException
+	{
+		CellValue cellValue = workbook.getCreationHelper().createFormulaEvaluator().evaluate(cell);
+		switch (cellValue.getCellTypeEnum())
+		{
+		case BOOLEAN:
+			return cellValue.getBooleanValue();
+		case NUMERIC:
+			return cellValue.getNumberValue();
+		case STRING:
+			return cellValue.getStringValue();
+		default:
+			throw new WorkbookDataException();
+		}
+	}
+}
